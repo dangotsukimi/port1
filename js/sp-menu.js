5780a410c7db662fb9f044023215bc498e82425f
dangotsukimi <dango_tsukimi@yahoo.co.jp>
2020-02-04 08:48:12 +0900

    dango

---
+++ js/sp-menu.js
@@ -0,0 +1,16 @@
+(function( $ ) {
+	function menu_open() {
+		if ( $( this ).hasClass( 'sp-close' ) ) {
+			$( this )
+				.removeClass( 'sp-close' )
+				.addClass( 'sp-open' );
+			$( '#menu' ).slideDown( 'fast' );
+		} else {
+			$( this )
+				.removeClass( 'sp-open' )
+				.addClass( 'sp-close' );
+			$( '#menu' ).slideUp( 'fast' );	
+		}	
+	}
+	$( '#sp-icon' ).on( 'click', menu_open );
+});
